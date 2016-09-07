2e52bf71d14775876482cf63bb5bf8a367c8f588
miktim <miktim@mail.ru>
2016-09-07 12:57:45 +0400

    Delete ServerStart.sh

--- ServerStart.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SCRIPTPATH=$BASH_SOURCE
-#SCRIPTPATH=$(dirname "$SCRIPT")
-SCRIPTPATH=$(dirname "$0")
-#echo "${SCRIPTPATH}"
-cd "${SCRIPTPATH}"/Linux
-java -DdavEnable=yes -jar ../RESTupServer.jar 
