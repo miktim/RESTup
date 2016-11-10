2fb96ddf16871bd096f371eab2228cde70950d9c
miktim <miktim@mail.ru>
2016-11-10 15:57:58 +0400

    Delete buildAgentTest.sh

--- Source/JavaAgent/buildAgentTest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-javac -cp ./restupAgent.jar -d ./org  RESTupAgentTest.java
-if [ $? -eq 0 ] ; then
-  echo java -cp ./:./org/ RESTupAgentTest $1
-  java -cp ./:./org/ RESTupAgentTest $1
-fi
